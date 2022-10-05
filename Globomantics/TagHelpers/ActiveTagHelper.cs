2ffc17d7aa2c660ce34c4739b841a9046f777cd2
German775 <German@DESKTOP-77O02G9>
2022-10-05 23:30:29 +0300

    Added active-url helper for <a> tag

---
+++ Globomantics/TagHelpers/ActiveTagHelper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Globomantics.TagHelpers
+{
+    [HtmlTargetElement("a", Attributes = "active-url")]
+    public class ActiveTagHelper : TagHelper
+    {
+        public string ActiveUrl { get; set; }
+        private IHttpContextAccessor _httpService;
+
+        public ActiveTagHelper(IHttpContextAccessor httpService)
+        {
+            _httpService = httpService;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (_httpService.HttpContext.Request.Path.ToString().Contains(ActiveUrl))
+            {
+                var exsitingAttrs = output.Attributes["class"]?.Value;
+                output.Attributes.SetAttribute("class", "active " + exsitingAttrs.ToString());
+            }
+        }
+    }
+}
