44949f28d91fc49078b2c3aad8fe4da0f38e1f5b
German775 <German@DESKTOP-77O02G9>
2022-11-11 01:21:57 +0300

    Added location expander for theme

---
+++ Globomantics/Theme/ThemeExpander.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Globomantics.Theme
+{
+    public class ThemeExpander : IViewLocationExpander
+    {
+        public IEnumerable<string> ExpandViewLocations(ViewLocationExpanderContext context, IEnumerable<string> viewLocations)
+        {
+            var activeTheme = "Beta";
+            var expandedLocations = viewLocations.ToList();
+
+            for (int i = 0; i < viewLocations.Count(); i++)
+            {
+                expandedLocations.Insert(i, viewLocations.ElementAt(i)
+                    .Replace("/Views/", string.Format("/Views/Themes/{0}/", activeTheme)));
+            }
+
+            return expandedLocations;
+        }
+
+        public void PopulateValues(ViewLocationExpanderContext context)
+        {
+            //throw new System.NotImplementedException();
+        }
+    }
+}
